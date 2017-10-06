409ff892235db992d058cb442f342e76809b7ee1
Raksha <raksha.atnoor@gmail.com>
2017-10-06 10:34:07 +0000

    added classes

--- app/data/FeedResponse.java
+++ app/data/FeedResponse.java
@@ -1,5 +1,7 @@
 package data;
 
 public class FeedResponse {
+   public  String title;
+    public String descrpition;
 
 }
--- app/data/Message.java
+++ app/data/Message.java
@@ -2,4 +2,10 @@ package data;
 
 public class Message {
 
+    public String text;
+
+    public enum Sender {BOT, USER};
+
+    public String time;
+    public Sender sender;
 }
