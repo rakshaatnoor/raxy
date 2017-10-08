908c4c87ec21c3761f9de7c27642ad351c4453c0
Raksha <raksha.atnoor@gmail.com>
2017-10-08 05:33:36 +0000

    convo done

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,26 @@
-
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'hi',
+            'time': '10:01 AM'
+        },
+        {
+            'sender': 'BOT',
+            'text': 'What can i do for you ? ',
+            'time': '10:01 AM'
+        },
+        {
+            'sender': 'USER',
+            'text': 'News about North Korea',
+            'time': '10:01 AM'
+        },
+        {
+            'sender': 'BOT',
+            'text': 'INVALID',
+            'time': '10:02 AM'
+        }
+    ];
 });
